beba37555ccee0a188b67f822533a5942cae8a30
Sungmin Kim <sungmin777-eng@example.com>
2025-07-22 11:16:27 +0900

    7.22

--- src/권오흠님 강의/counting sort.ts
+++ src/권오흠님 강의/counting sort.ts
@@ -6,11 +6,13 @@ function countingSort(arr: number[], maxValue: number): number[] {
   for (let i = 0; i < arr.length; i++) {
     count[arr[i]]++;
   }
+  console.log(count)
 
   // 2. 누적 합 배열 만들기 (stable 정렬 위해)
   for (let i = 1; i <= maxValue; i++) {
     count[i] += count[i - 1];
   }
+  console.log(count)
 
   // 3. 출력 배열 만들기 (뒤에서부터 → 안정성 유지)
   for (let i = arr.length - 1; i >= 0; i--) {
